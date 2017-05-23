abe9a6a585d573f86a8c44bd8f9643462a9dc03a
Allan Liu <aliu@digitalocean.com>
2017-05-23 13:29:27 -0400

    smart_drives: bump libsmartctl version to v0.2.2

--- tools/provision/formula/smartmontools.rb
+++ tools/provision/formula/smartmontools.rb
@@ -3,8 +3,8 @@ require File.expand_path("../Abstract/abstract-osquery-formula", __FILE__)
 class Smartmontools < AbstractOsqueryFormula
   desc "SMART hard drive monitoring; Fork with smartctl exposed as a static library"
   homepage "https://www.smartmontools.org/"
-  url "https://github.com/allanliu/smartmontools/archive/v0.2.1.tar.gz"
-  sha256 "8529d0f4f87ff3c73a96215f75110800bcc25982b69f06d3e82a24e10e625f47"
+  url "https://github.com/allanliu/smartmontools/archive/v0.2.2.tar.gz"
+  sha256 "e8b43194d3c967fc09b99e883da6b57f70e979ffd3bde8c456a9bde58e357e32"
 
 
   depends_on "automake" => :build
